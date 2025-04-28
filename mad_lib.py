915fcad5b7cab3379c8cc48d08df78c886a6ff45
Venkateshpatel20 <venkateshpatel18@gmail.com>
2025-04-28 22:49:54 +0530

    Add files via upload
    This hangman game is a string based game where the user will guess the a word .

---
+++ mad_lib.py
@@ -0,0 +1,13 @@
+#youtuber
+# youtuber = "patel"
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"suscribe to {youtuber}")
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+Famous_person = input("Famous person: ")
+madlib = f"Computer programming is so {adj}! It makes me \
+so excited all the time because I love to {verb1}.\
+Stay hydrated and {verb2} like your {Famous_person}!"
+print(madlib)
